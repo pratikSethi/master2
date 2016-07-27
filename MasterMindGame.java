f75e0b268e2bf1bdc54e13e8856ca08cfdda7599
iambhavin <bhavin_patel666@yahoo.com>
2016-07-27 12:49:16 +0530

    Add files via upload

--- MasterMindGame.java
+++ MasterMindGame.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
+/**
+ * Created by patelbh on 7/27/2016.
+ */
 public class MasterMindGame {
+    public static void main(String[] args) {
+
+        int difficulty = getDifficulty();
+    }
+
+    public static String getWord (int diff)
+    {
+        System.out.println("Enter" +diff+ "letter word");
+        String s = new String();
+        Scanner sc  = new Scanner(System.in());
+        s = sc.next();
+        while(s.length() != diff)
+        {
+            System.out.println("Enter correct word");
+            s = sc.next();
+        }
+        System.out.println(s);
+        return s;
+    }
+
 
 }
