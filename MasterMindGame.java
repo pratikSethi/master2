4159ee8fe4860edaf03657d2f72ba4e9e279e9cf
chiragrtr <chiragrtr@gmail.com>
2016-07-27 12:58:33 +0530

    added method to get a unique String

--- MasterMindGame.java
+++ MasterMindGame.java
@@ -1,34 +1,51 @@
-import com.sun.org.apache.xpath.internal.SourceTree;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
 
-import java.util.Scanner;
-import java.util.concurrent.SynchronousQueue;
-
-/**
- * Created by torap on 7/27/2016.
- */
 public class MasterMindGame {
-    public static void main(String[] args) {
-        int difficulty=getDifficulty();
-        getWordFromUSer(difficulty);
-    }
-    public static int getDifficulty(){
-        int diff;
-        int choice=1;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter Difficulty between 3 to 5");
-        diff=sc.nextInt();
-        return diff;
-    }
-
-    public static String getWordFromUSer(int diff) {
-        System.out.println("enter"+diff+" letter word");
-        String s=new String();
-        Scanner sc=new Scanner(System.in);
-        s=sc.next();
-        while(s.length()!=diff) {
-            System.out.println("enter correct word");
-            s=sc.next();
+    public static String wordWithUniqueLetters(int difficulty){
+        ArrayList<String> lines =getLines("src\\sowpods.txt");
+        ArrayList<String> words = new ArrayList<>();
+        for (String line : lines){
+            String[] wordsInLine = getWords(line);
+            for(String string : wordsInLine) {
+                words.add(string);
+            }
         }
-        return s;
+        int randomNum = (int) (Math.random() * 50 + 1);
+        String requiredWord = words.get(randomNum);
+        return requiredWord;
+    }
+    public static String[] getWords(String line) {
+        return line.replaceAll("[[^a-zA-Z]&&[^a-zA-z'a-zA-Z]]", " ").split(" +");
+    }
+    public static ArrayList<String> getLines (String filename){
+        File f = new File(filename);
+        BufferedReader br = null;
+        ArrayList<String> lineArray = new ArrayList<>();
+        try{
+            FileReader fr = new FileReader(f);
+            br = new BufferedReader(fr);
+            String line;
+            while((line = br.readLine()) != null){
+                lineArray.add(line);
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("error");
+        }
+        catch (IOException e){
+            System.out.println("can't read");
+        }
+        try{
+            br.close();
+        }
+        catch(IOException e){
+            System.out.println("can't close");
+        }
+        catch(NullPointerException e){
+            System.out.println("File never opened");
+        }
+        return lineArray;
     }
 }
