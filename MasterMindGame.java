317cf9efcc0c9670872ce3a99a2a768630bf8306
chiragrtr <chiragrtr@gmail.com>
2016-07-27 12:50:27 +0530

    added method to get a unique String

--- MasterMindGame.java
+++ MasterMindGame.java
@@ -1,28 +1,51 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
 
-/**
- * Created by patelbh on 7/27/2016.
- */
 public class MasterMindGame {
-    public static void main(String[] args) {
-
-        int difficulty = getDifficulty();
-    }
-
-    public static String getWord (int diff)
-    {
-        System.out.println("Enter" +diff+ "letter word");
-        String s = new String();
-        Scanner sc  = new Scanner(System.in());
-        s = sc.next();
-        while(s.length() != diff)
-        {
-            System.out.println("Enter correct word");
-            s = sc.next();
+    public static String wordWithUniqueLetters(int difficulty){
+        ArrayList<String> lines =getLines("src\\sowpods.txt");
+        ArrayList<String> words = new ArrayList<>();
+        for (String line : lines){
+            String[] wordsInLine = getWords(line);
+            for(String string : wordsInLine) {
+                words.add(string);
+            }
         }
-        System.out.println(s);
-        return s;
+        int randomNum = (int) (Math.random() * 50 + 1);
+        String requiredWord = words.get(randomNum);
+        return requiredWord;
     }
-
-
+    public static String[] getWords(String line) {
+        return line.replaceAll("[[^a-zA-Z]&&[^a-zA-z'a-zA-Z]]", " ").split(" +");
+    }
+    public static ArrayList<String> getLines (String filename){
+        File f = new File(filename);
+        BufferedReader br = null;
+        ArrayList<String> lineArray = new ArrayList<>();
+        try{
+            FileReader fr = new FileReader(f);
+            br = new BufferedReader(fr);
+            String line;
+            while((line = br.readLine()) != null){
+                lineArray.add(line);
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("error");
+        }
+        catch (IOException e){
+            System.out.println("can't read");
+        }
+        try{
+            br.close();
+        }
+        catch(IOException e){
+            System.out.println("can't close");
+        }
+        catch(NullPointerException e){
+            System.out.println("File never opened");
+        }
+        return lineArray;
+    }
 }
